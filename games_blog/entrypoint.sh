da7be9703684b3f0247f7f465d167b7d03119b57
qustoo <qustoo@yandex.ru>
2024-12-19 02:49:52 +0300

    wait for create db

---
+++ games_blog/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Проверка работы базы данных..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Postgres еще не запущен..."
+
+    # Проверяем доступность хоста и порта
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 10
+    done
+
+    echo "PostgreSQL запущен"
+fi
+
+exec "$@"
+
+
